0bac2a935606dc545705d0024edd767c1f29e703
Wellington Santos <w.pereira.santos@avanade.com>
2021-03-10 16:27:03 -0300

    Enviando os dados pelo form

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 
 const Pesquisa = () => {
+    const [form, setForm] = useState({})
+    
+    const handleChange = (e) => {
+        const valor = e.target.value
+        const name = e.target.name
+        setForm(old => ({
+            ...old,
+            [name]: valor
+        }))
+    }
 
     const salvar = async () => {
-        console.log("Botão salvar");
-        const form = {
-            Nome: "Wellington Test",
-            Email: "test@test.com",
-            Whatsapp: "21995599868"
-        }
+        
         try {
             const response = await fetch('/api/post-save', {
                 method: "POST",
@@ -18,6 +23,7 @@ const Pesquisa = () => {
 
             const data = await response.json()
             console.log(data);
+            
         } catch (error) { }
     }
 
@@ -27,15 +33,18 @@ const Pesquisa = () => {
             <p className='text-center mb-8'>O restaurante [Nomde do Restaurante] sempre buscar por melhorar o seu atendimento e seus produtos</p>
             <div className='w-1/5 mx-auto'>
                 <label htmlFor="" className='font-bold'>Seu nome:</label>
-                <input className='p-4 block shadow bg-blue-100 my-2 rounded' type="text" />
+                <input className='p-4 block shadow bg-blue-100 my-2 rounded' type="text" name="Nome" onChange={handleChange} valule={form.Nome} />
                 <label htmlFor="" className='font-bold'>Seu telefone:</label>
-                <input className='p-4 block shadow bg-blue-100 my-2 rounded' type="tel" />
+                <input className='p-4 block shadow bg-blue-100 my-2 rounded' type="tel" name="Whatsapp" onChange={handleChange} valule={form.Whatsapp} />
                 <label htmlFor="" className='font-bold'>Seu melhor e-mail:</label>
-                <input className='p-4 block shadow bg-blue-100 my-2 rounded' type="email" />
+                <input className='p-4 block shadow bg-blue-100 my-2 rounded' type="email" name="Email" onChange={handleChange} valule={form.Email} />
                 <label htmlFor="" className='font-bold'>Deixe sua opinião ou sugestão</label>
-                <input className='p-4 block shadow bg-blue-100 my-2 rounded' type="textarea" />
+                <input className='p-4 block shadow bg-blue-100 my-2 rounded' type="textarea" name="Sugestao" onChange={handleChange} valule={form.Sugestao} />
                 <button onClick={() => salvar()}>Salvar</button>
             </div>
+            <pre>
+                {JSON.stringify(form, null, 2)}
+            </pre>
         </div>
     )
 }
